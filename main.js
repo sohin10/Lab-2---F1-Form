526f2239686a30797dfe380c449c9ca87df0f99c
sohin10 <sohinipandit10@gmail.com>
2022-09-20 08:41:22 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// const button = document.querySelector('#my-button');
+// button.addEventListener('click', (){
+//     //process form here
+//     const firstTextInput =  document.querySelector('#first-text-input');
+//     console.log(firstTextInput.value);
+
+    //color input
+    const color = document.querySelector('.colors');
+    const colorInput = document.querySelector('color-input');
+    form.style.backgroundColor = colorInput.value;
+    colorInput.value;
+
+    // colorInput.addEventListener('input', () => {
+    //     color.style.backgroundColor = colorInput.value;
+    // })
+
+
+    // //checkboxes input
+    // const checkboxesElement = document.querySelector('#checkboxes');
+    // const checkboxes = checkboxesElement.querySelectorAll('input');
+    // for(const checkbox of checkboxes){
+    //     console.log(checkbox.checked);
+
+    // }
+  
+// })
