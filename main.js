d248afd9c77f16cc3f83ecfe32622943a536e33e
sohin10 <sohinipandit10@gmail.com>
2022-09-20 15:52:03 -0600

    Radio buttons, text box
    added colors, edited radio buttons, hover buttons

--- main.js
+++ main.js
@@ -1,26 +1,6 @@
-// const button = document.querySelector('#my-button');
-// button.addEventListener('click', (){
-//     //process form here
-//     const firstTextInput =  document.querySelector('#first-text-input');
-//     console.log(firstTextInput.value);
+let color = document.querySelector('.color');
+let tshirt = document.querySelector('.tshirt');
 
-    //color input
-    const color = document.querySelector('.color');
-    const colorInput = document.querySelector('color-input');
-    // form.style.backgroundColor = colorInput.value;
-    // colorInput.value;
-
-    colorInput.addEventListener('input', () => {
-        color.style.backgroundcolor = colorInput.value;
-    })
-
-
-    // //checkboxes input
-    // const checkboxesElement = document.querySelector('#checkboxes');
-    // const checkboxes = checkboxesElement.querySelectorAll('input');
-    // for(const checkbox of checkboxes){
-    //     console.log(checkbox.checked);
-
-    // }
-  
-// })
+color.addEventListener('input', ()=>{
+    tshirt.style.background = color.value;
+})
